dcab9575af28c3cd6e63957c9e5a9efc897c54d9
suyeong-jeong <sy.jeong100@gmail.com>
2025-04-01 21:48:43 +0900

    pes1

---
+++ pes1.c
@@ -0,0 +1,32 @@
+////11번
+//#include <stdio.h>
+//int main(void)
+//{
+//	int a = 100;
+//	float b = -0.5;
+//	char c = 'I';
+//	char str[15] = "can do it!";
+//
+//	printf("%d\n", a);
+//	printf("%.6lf\n", b);
+//	printf("%c\n", c);
+//	printf("%s\n", str);
+//
+//	return 0;
+//}
+
+
+////12번
+//#include <stdio.h>
+//int main(void){
+//	float num1, num2;
+//	printf("두 실수 입력: ");
+//	scanf_s("%f %f", &num1, &num2);
+//
+//	printf("%.6f\n", num1 + num2);
+//	printf("%.6f\n", num1 - num2);
+//	printf("%.6f\n", num1 * num2);
+//	printf("%.6f\n", num1 / num2);
+//
+//	return 0;
+//}
